df44cb7bf7ac94ad125c1db5d17f3ad66e4f8828
sairam0424 <uggesairam0000@gmail.com>
2024-08-16 11:17:21 +0530

    Board Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Square from './components/Square';
+import logo from "./logo.svg";
+import "./App.css";
+import './styles.css'
+import Square from "./components/Square";
+import Board from "./components/Board";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Square/>
+        {/* <Square /> */}
 
+        <Board/>
       </header>
     </div>
   );
