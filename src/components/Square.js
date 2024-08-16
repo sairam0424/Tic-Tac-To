467e4c539addd286c4a0bd7dd9cb522ae5d21fb4
sairam0424 <uggesairam0000@gmail.com>
2024-08-16 11:10:13 +0530

    create board

---
+++ src/components/Square.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Square() {
+  return (
+    <>
+    <div className='board-row'>
+
+        <button className='square1'>1</button>
+        <button className='square2'>2</button>
+        <button className='square3'>3</button>
+
+    </div>
+    <div className='board-row'>
+
+        <button className='square4'>4</button>
+        <button className='square5'>5</button>
+        <button className='square6'>6</button>
+        
+    </div>
+    <div className='board-row'>
+
+        <button className='square7'>7</button>
+        <button className='square8'>8</button>
+        <button className='square9'>9</button>
+        
+    </div>
+
+
+
+
+
+    </>
+  )
+}
+
+export default Square
